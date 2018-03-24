953869fd182b65fd23316873794be426aac21efa
Ankit <aankittcoolest@gmail.com>
2018-03-24 23:41:25 +0900

    Fixing CORS

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
+
 app.use('/user', userRouters)
 app.use('/sender-addresses', senderAddressesRouters)
 app.use('/receiver-addresses', receiverAddressesRouters)
